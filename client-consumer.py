caa22e0cd844260112047646749d66daf5dd5cdf
Tom Caspy <tom@Miris-MBP.tikalk.com>
2020-04-22 14:08:05 +0300

    second commit, still not much to see here

--- client-consumer.py
+++ client-consumer.py
@@ -1,12 +1,15 @@
 import pulsar
+import json
 
 client = pulsar.Client('pulsar://localhost:6650')
 consumer = client.subscribe('tweets',
-                            subscription_name='twitter_feed')
+                            subscription_name='tweets')
 
 while True:
     msg = consumer.receive()
     print("Received message: '%s'" % msg.data())
+    json_data = json.loads(msg.data())
+    print(json_data)
     consumer.acknowledge(msg)
 
 client.close()
--- step1.py
+++ step1.py
@@ -1,9 +1,16 @@
+import json
 from pulsar import Function
 
-class Step1(Function):
+class FilterEnglish(Function):
     def __init__(self):
-        self.twitter_topic = "persistent://public/default/twitter"
+        self.twitter_topic = "tweets"
+        self.foo_topic = "foo"
+        self.bar_topic = "bar"
 
     def process(self, item, context):
-        warning = "The item {0}".format(item)
-        context.get_logger().warn(warning)
+    	json_item = json.loads(item)
+    	if json_item['lang'] is not None and json_item['lang'] == 'en':
+        	context.publish(self.foo_topic, json.dumps(json_item))
+        else:
+        	context.publish(self.bar_topic, json.dumps(json_item))
+
